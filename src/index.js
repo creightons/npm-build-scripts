5741b19df8f95fa78f4c8ed5d6e24ef3aacfff94
Creighton <SCreighton@tecogen.local>
2017-02-28 10:02:57 -0500

    Added more options for building content, including more browserify transforms and adding source maps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
+
 var constants = require('./constants');
 
 const init = () => { console.log(constants, Math.random() * 100) } ;
 init();
+
+var environment = process.env.PATH;
+
+console.log(environment);
+
+console.log('jojojo');
